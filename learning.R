7b1050cf60fc69044fcfa0b5620c3edbd91ce9bf
Adnan Adeeb <158167714+adnanadeeb@users.noreply.github.com>
2024-02-25 23:06:44 +0530

    Create learning.R

---
+++ learning.R
@@ -0,0 +1,29 @@
+a=64856732
+b=678987654
+a+b
+b-a
+65^67
+ c(1,2,8,3,4,5)
+v=c(1,2,8,3,4,5) 
+v
+head(v)
+tail(v)
+5:17
+v6=c(5:13)
+v6
+seq(2,8, by= .5)
+seq(0,1, length= 11)
+v1=c(2,4,3,0,8,5)
+v2=c(3,5,4,1,5,8)
+v1+v2
+v1-v2
+v1*v2
+v1/v2
+# element recycle 
+v1=c(2,4,6,8,4,6)
+v2=c(2,5)
+v1+v2
+# vector sorting 
+v=c(2,5,3,7,-9,304)
+sort(v)
+sort(v ,decreasing = F)
